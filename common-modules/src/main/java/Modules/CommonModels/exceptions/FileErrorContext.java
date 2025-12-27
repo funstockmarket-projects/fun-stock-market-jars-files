2598e5d587760e5a2f670995089c9786c67fda8b
katuk <funstockmarket365@gmail.com>
2025-12-27 19:49:53 +0530

    added new project

--- common-modules/src/main/java/Modules/CommonModels/exceptions/FileErrorContext.java
+++ common-modules/src/main/java/Modules/CommonModels/exceptions/FileErrorContext.java
@@ -1,4 +1,16 @@
 package Modules.CommonModels.exceptions;
 
+import Modules.CommonModels.enums.ErrorCodes;
+
 public class FileErrorContext extends Exception{
+
+    public FileErrorContext(String e) {
+        super(e);
+    }
+
+    public  FileErrorContext(ErrorCodes code){
+        super(code.toString());
+    }
+
+
 }
